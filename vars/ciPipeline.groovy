08c93d49d0abd9dd0639bc61071a1f84cd787063
mikey <mikey@darasa.io>
2025-05-16 14:48:17 +0400

    update jenkins file

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,19 +1,13 @@
 def call(Map config = [:]) {
     pipeline {
-        agent any
+        agent none
 
         stages {
-            stage('Init') {
-                steps {
-                    script {
-                        echo "Language: ${config.language}"
-                    }
-                }
-            }
-
             stage('Build') {
-                steps {
+                agent {label 'build'}
+                steps {    
                     script {
+                        checkoutByTag(repoUrl, tag, String credentialsId = 'default-cred')
                         if (config.language == 'java') {
                             buildJava(config)
                         } else if (config.language == 'php') {
@@ -21,9 +15,56 @@ def call(Map config = [:]) {
                         } else {
                             error "Unsupported language: ${config.language}"
                         }
+                    
+                }
+            }
+            stage('Push to s3/ecr/arifact') {
+                agent {label 'build'}
+                steps {    
+                    script {
+                        sh 'echo "Push code"'
+                    }
+                }
+            }
+            stage('update upstream proxy') {
+                agent {label 'build'}
+                steps {    
+                    script {
+                        sh 'echo "canary deployment"'
+                    }
+                }
+            }
+            stage('deploy to prd server') {
+                agent {label 'build'}
+                steps {    
+                    script {
+                        sh 'echo "download source from repository"'
+                        sh 'generation configuration'
+                        sh 'restart services'
+                        sh 'healcheck function'
+                        sh 'update proxy'
+                    }
+                }
+            }
+            stage('clean') {
+                agent {label 'build'}
+                steps {    
+                    script {
+                        sh 'echo "clean"'
                     }
                 }
             }
         }
     }
 }
+def checkoutByTag(String repoUrl, String tag, String credentialsId = 'default-cred') {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "refs/tags/${tag}"]],
+        userRemoteConfigs: [[
+            url: repoUrl,
+            credentialsId: credentialsId
+        ]]
+    ])
+}
+
